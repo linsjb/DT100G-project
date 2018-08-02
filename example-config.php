cf370c7557d0e18117956b6daf860c27a59d9489
linsjb <linussjobro@gmail.com>
2018-08-02 16:36:58 +0200

    Added example config file

---
+++ example-config.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* File name: config.php
+* Author: Linus Sjöbro
+* E-mail: lisj1502@student.miun.se
+*
+* Description:
+* Hold's the login-information for the database.
+*/
+
+define("DB_SERVERNAME", "");
+define("DB_NAME", "");
+define("DB_USER", "");
+define("DB_PASSWORD", "");
+?>
